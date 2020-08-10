00695157a5033bfa1ddef3c1aef43fe857b21c9d
Andres Celis <adwher@outlook.com>
2020-08-10 15:32:33 -0500

    first commit

---
+++ Testing.Test/FirstTest.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Testing.Test
+{
+    [TestClass]
+    public class FirstTest
+    {
+        [TestMethod]
+        public void VariableIsTrue()
+        {
+            var variable = true;
+            Assert.IsTrue(variable);
+        }
+    }
+}
